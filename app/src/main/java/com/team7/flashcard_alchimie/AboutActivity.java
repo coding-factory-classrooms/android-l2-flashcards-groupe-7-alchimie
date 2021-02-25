9f38faa36a836380d8fe5ddae11798c10ee724ec
Erwan <erwan.spenette@icloud.com>
2021-02-25 15:45:55 +0100

    Story 11 done à test

---
+++ app/src/main/java/com/team7/flashcard_alchimie/AboutActivity.java
@@ -0,0 +1,27 @@
+package com.team7.flashcard_alchimie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Intent srcIntent = getIntent();
+
+        String versionName = BuildConfig.VERSION_NAME;
+        TextView versionTextView = findViewById(R.id.versiontextView);
+        versionTextView.setText(versionName);
+
+    }
+
+
+
+
+}
